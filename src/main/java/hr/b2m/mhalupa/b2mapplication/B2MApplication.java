ab15b564e182124766cf846751d31f8cf00062ad
Matej Halupa <matej.halupa@gmail.com>
2022-03-10 11:09:04 +0100

    init

---
+++ src/main/java/hr/b2m/mhalupa/b2mapplication/B2MApplication.java
@@ -0,0 +1,13 @@
+package hr.b2m.mhalupa.b2mapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2MApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(B2MApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hr/b2m/mhalupa/b2mapplication/B2MApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.b2m.mhalupa.b2mapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2MApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
